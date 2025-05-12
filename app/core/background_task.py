a001b4cfa6571b5998df3e07a89f77496f2f6306
Georgi Iliev <georgi.iliev533@outlook.com>
2025-05-12 23:09:36 +0300

    Added business logic to background task

--- app/core/background_task.py
+++ app/core/background_task.py
@@ -1,3 +1,47 @@
-from app.models import SharedState, Process
-async def background_worker(state: SharedState):
+import psutil
+import asyncio
+from collections import deque
+from typing import Literal, Annotated
 
+from app.models import SharedState, Process
+from app.core.config import settings
+
+
+async def background_worker(state: SharedState):
+    cpu_count = psutil.cpu_count(logical=True)
+
+    while True:
+        processes_list = []
+
+        processes = {p.pid: p for p in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent'])}
+        for proc in processes.values():
+            try:
+                proc.cpu_percent(interval=None)
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                continue
+
+        await asyncio.sleep(5)
+
+        for pid, proc in processes.items():
+            try:
+                with proc.oneshot():
+                    process = Process(
+                        pid=proc.pid,
+                        name=proc.name(),
+                        cpu_usage=proc.cpu_percent(interval=0) / cpu_count,
+                        memory_usage=proc.memory_percent(),
+                        status=proc.status()
+                    )
+                    processes_list.append(process)
+                    key = (process.pid, process.name)
+                    if key not in state.history:
+                        state.history[key] = {
+                            'cpu': deque(maxlen=settings.ROLLING_WINDOW_SIZE),
+                            'mem': deque(maxlen=settings.ROLLING_WINDOW_SIZE),
+                        }
+                    state.history[key]['cpu'].append(process.cpu_usage)
+                    state.history[key]['mem'].append(process.memory_usage)
+
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                continue
+        state.processes = processes_list
--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
+from typing import Optional, Literal
+
+from pydantic import BaseModel, computed_field
+
+
+class Process(BaseModel):
+    pid: int
+    name: str
+    cpu_usage: float
+    memory_usage: float
+    status: str
+
+
+class ProcessList(BaseModel):
+    processes: list[Process]
+
+    @computed_field
+    @property
+    def size(self) -> int:
+        return len(self.processes)
+
+
 class SharedState:
     def __init__(self):
         self.processes = []
