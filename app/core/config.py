dcb6e308c8325329195575b7cbb2c89bb19e3554
Georgi Iliev <georgi.iliev533@outlook.com>
2025-05-13 19:29:45 +0300

    Added cors to env var

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,15 @@
-from typing import Literal
+from typing import Literal, Any, Annotated
 
+from pydantic import AnyUrl, BeforeValidator, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",")]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
 
 
 class Settings(BaseSettings):
@@ -19,6 +28,17 @@ class Settings(BaseSettings):
     ANOMALY_THRESHOLD: float = 3.0
     ROLLING_WINDOW_SIZE: int = 30
     ROLLING_WINDOW_LIMIT: int = 5
+
+    CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def all_cors_origins(self) -> list[str]:
+        if self.ENVIRONMENT == "local":
+            return ["*"]
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS]
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import processes, anomaly
 from app.core.config import settings
@@ -9,6 +10,14 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     description=settings.PROJECT_DESCRIPTION,
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.all_cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(processes.router)
